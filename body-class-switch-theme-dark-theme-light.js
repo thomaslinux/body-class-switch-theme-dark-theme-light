9692dcaadafe512baf8c4111539ce18a1e330dc1
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-13 11:36:35 +0100

    Create body-class-switch-theme-dark-theme-light.js

---
+++ body-class-switch-theme-dark-theme-light.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        body-class-switch-theme-dark-theme-light
+// @namespace
+// @match       https://www.eni-training.com/portal/client/mediabook/home*
+// @match       https://www.eni-training.com/*
+// @match       *://*/*
+// @grant       none
+// @version     2026.02.13.09.56
+// @author      thomaslinux
+// @description body-class-switch-theme-dark-theme-light
+// ==/UserScript==
+// document.body.className = document.body.className.replace("theme-light","theme-dark")
+
+// document.body.className = document.body.className.replace("theme-dark","theme-light")
+
+window.onload = (function() {
+    'use strict';
+
+    function toggleTheme() {
+        if (document.body.className.includes("theme-dark")) {
+            document.body.className = document.body.className.replace("theme-dark", "theme-light");
+        } else {
+            document.body.className = document.body.className.replace("theme-light", "theme-dark");
+        }
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.ctrlKey && event.key === 'y') {
+            toggleTheme();
+        }
+    });
+
+})();
